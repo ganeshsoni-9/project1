ab451d5581fa813e9ac7e52435fc58725ef673c8
ganeshsoni-9 <ganeshsonirmg18@gmail.com>
2025-07-01 20:05:01 +0530

    Add files via upload

---
+++ 1-july divisible by 5 and 11 js    5..js
@@ -0,0 +1,8 @@
+let number = 90;
+
+if(number % 5 === 0 && number % 11 === 0){
+    console.log(number + "divisible by both 5 and 11");
+}
+ else {
+    console.log(number + "both are divisible by 5 and 11")
+}
