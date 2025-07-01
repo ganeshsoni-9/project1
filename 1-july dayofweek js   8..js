fd0c3e97881882b2cf843c016153ee9eb23c40d6
ganeshsoni-9 <ganeshsonirmg18@gmail.com>
2025-07-01 20:06:26 +0530

    Add files via upload

---
+++ 1-july dayofweek js   8..js
@@ -0,0 +1,33 @@
+let day =2;
+
+if(day === 1){
+    console.log("monday")
+}
+
+else if(day === 2){
+    console.log("tuesday")
+}
+
+else if(day === 3){
+    console.log("Wednesday")
+}
+
+else if(day === 4){
+    console.log("thursday")
+}
+
+else if(day === 5){
+    console.log("Friday")
+}
+
+else if(day === 6){
+    console.log("Saturday")
+}
+
+else if(day === 7){
+    console.log("Sunday")
+}
+
+else {
+    console.log("invalid")
+}
