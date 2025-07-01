077a1ee606dd09ddde7a23ce62d43f705be09a7c
ganeshsoni-9 <ganeshsonirmg18@gmail.com>
2025-07-01 20:04:05 +0530

    Add files via upload

---
+++ 1-july greatest of two number js  3..js
@@ -0,0 +1,10 @@
+let number1 = 20;
+let number2 = 40;
+
+if(number1 > number2) {
+    console.log(number1 + "greater than" + number2);
+} else if (number2 > number1) {
+    console.log(number2 + "greater than" + number1);
+    } else {
+        console.log("both are equal");
+    }
