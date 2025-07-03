94a1c1c251c5c9ce5d74b799c228f54449a31c21
ganeshsoni-9 <ganeshsonirmg18@gmail.com>
2025-07-03 15:36:31 +0530

    Delete 2-july array.js.js

--- 2-july array.js.js
+++
@@ -1,7 +0,0 @@
-let a = [12 , 'a' , true]
-let b = [2,5,7,8,5,4]
-//console.log(b.push(12,45))
-//console.log(b.pop())
-//console.log(b.shift())
-//console.log(b.unshift())
-console.log(b.slice(1,4))
