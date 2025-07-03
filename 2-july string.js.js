3fe9b915998027147f35740dabe0b7748dfcf45d
ganeshsoni-9 <ganeshsonirmg18@gmail.com>
2025-07-03 11:21:10 +0530

    Add files via upload

---
+++ 2-july string.js.js
@@ -0,0 +1,19 @@
+let a = "hello";
+let b = "Javascript"
+let c = "banana"
+let d = "frontened-backened"
+//console.log(a.toUpperCase('hello'))//
+//console.log(b.toLowerCase('Javascript'))//
+//console.log(a.trim('hello'))//
+//console.log(a.replace('h' , 'z'))//
+//console.log(a.split("ho"))//
+//console.log(a.endswith("lo"))
+//console.log(a.repeat("3"))
+//console.log(a.substring("hello"))
+//console.log(a.split("lo"))
+//console.log(d.split("fr"))
+//console.log(a.padStart(2 , "*"))
+//console.log(a.padEnd(3, "*"))
+
+
+
