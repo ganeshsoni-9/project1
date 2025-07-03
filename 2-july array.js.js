05db1f71246cc1e399884e8f0146816b09fa9306
ganeshsoni-9 <ganeshsonirmg18@gmail.com>
2025-07-03 11:20:25 +0530

    Add files via upload

---
+++ 2-july array.js.js
@@ -0,0 +1,7 @@
+let a = [12 , 'a' , true]
+let b = [2,5,7,8,5,4]
+//console.log(b.push(12,45))
+//console.log(b.pop())
+//console.log(b.shift())
+//console.log(b.unshift())
+console.log(b.slice(1,4))
